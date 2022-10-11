3634bc58e8c290de0bd244714f3980ee3f705200
StevCon <hans@ai-blox.com>
2022-10-11 16:25:05 +0200

    Add hub + ethernet adapter test

--- src/units/lan9514.py
+++ src/units/lan9514.py
@@ -10,8 +10,8 @@ class Lan9514(BaseUnit):
 
         self.logger.info('Initialize LAN9514 test unit')
 
-        self.bus = config.get('bus', 0)
-        self.device = config.get('device', 0)
+        self.bus_id = config.get('bus', 0)
+        self.device_id = config.get('device', 0)
 
         self.usb = Usb()
 
@@ -21,16 +21,28 @@ class Lan9514(BaseUnit):
         self.nextState = self.state_1
 
     def state_1(self):
-        self.logger.info('Find LAN9514 device: bus {bus:03d}, device {device:03d}'.format(bus=self.bus, device=self.device))
-        self.device = self.usb.find_by_bus(self.bus, self.device)
-        if self.device:
+        self.logger.debug('Find LAN9514 Hub: bus {bus:03d}, device {device:03d}'.format(bus=self.bus_id, device=self.device_id))
+        device = self.usb.find_by_bus(self.bus_id, self.device_id)
+        if device:
+            self.logger.info('LAN9514 Hub OK')
             self.nextState = self.state_2
         else:
-            self.logger.error('Device not found!')
+            self.logger.error('LAN9514 Hub not found!')
             self.nextState = self.state_finish
 
     def state_2(self):
-        self.logger.info('Device found, implement next step')
+        self.device_id += 1
+        self.logger.debug('Find LAN9514 Ethernet adapter: bus {bus:03d}, device {device:03d}'.format(bus=self.bus_id, device=self.device_id))
+        device = self.usb.find_by_bus(self.bus_id, self.device_id)
+        if device:
+            self.logger.info('LAN9514 Ethernet Adapter OK')
+            self.nextState = self.state_3
+        else:
+            self.logger.error('LAN9514 Ethernet adapter not found')
+            self.nextState = self.state_finish
+
+    def state_3(self):
+        self.logger.debug('TODO: Implement next steps')
         self.nextState = self.state_finish
 
     def state_finish(self):
