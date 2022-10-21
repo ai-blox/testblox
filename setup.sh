a80bf0be56f9b018f3e8c4216ab56edb19afe86e
StevCon <hans@ai-blox.com>
2022-10-21 08:34:51 +0200

    change virtual env ai-blox

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ sudo su -c "apt-get -y install python3-pip"
 
 pip3 install virtualenv
 
-python3 -m virtualenv ~/venv-aiblox
-source ~/venv-aiblox/bin/activate
+python3 -m virtualenv ~/.venv-aiblox
+source ~/.venv-aiblox/bin/activate
 
 pip3 install -r requirments.txt
 
