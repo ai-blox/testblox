8e332d7a22d40ae3ba3fbf82879234faac13d639
Hans Stevens <hans@ai-blox.com>
2023-01-06 14:15:03 +0100

    fix async

--- scripts/s360/final.sh
+++ scripts/s360/final.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -O https://s360.s3.eu-central-1.amazonaws.com/tegra210-p3448-0002-s360v0-2-b00.dts 
-mv tegra210-p3448-0002-s360v0-2-b00.dts /boot/tegra210-p3448-0002-s360v0-2-b00.dts
-curl https://s360.s3.eu-central-1.amazonaws.com/set_mac.py --output set_mac.py
+curl -O https://s360.s3.eu-central-1.amazonaws.com/tegra210-p3448-0002-s360v0-2-b00.dts && 
+mv tegra210-p3448-0002-s360v0-2-b00.dts /boot/
+curl https://s360.s3.eu-central-1.amazonaws.com/set_mac.py --output set_mac.py &&
 python3 set_mac.py
 rm set_mac.py
 rm final.sh
