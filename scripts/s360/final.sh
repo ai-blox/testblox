fa413e0636fbe08074bb2a66d234ceeb1bd5d5b6
Hans Stevens <hans@ai-blox.com>
2023-01-06 15:48:20 +0100

    fix output file

--- scripts/s360/final.sh
+++ scripts/s360/final.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -o https://s360.s3.eu-central-1.amazonaws.com/tegra210-p3448-0002-s360v0-2-b00.dts
+curl -O https://s360.s3.eu-central-1.amazonaws.com/tegra210-p3448-0002-s360v0-2-b00.dts
 mv tegra210-p3448-0002-s360v0-2-b00.dts /boot/ 
-curl https://s360.s3.eu-central-1.amazonaws.com/set_mac.py --output set_mac.py 
+curl -O https://s360.s3.eu-central-1.amazonaws.com/set_mac.py
 python3 set_mac.py 
 rm set_mac.py
 rm final.sh
