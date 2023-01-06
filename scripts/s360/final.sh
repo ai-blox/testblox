9951d28543ea1815002faaaa2421ee2d3fd6917c
Hans Stevens <hans@ai-blox.com>
2023-01-06 12:44:34 +0100

    fix device tree

--- scripts/s360/final.sh
+++ scripts/s360/final.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-curl https://s360.s3.eu-central-1.amazonaws.com/tegra210-p3448-0002-s360v0-2-b00.dts --output /boot/tegra210-p3448-0002-s360v0-2-b00.dts   
+curl https://s360.s3.eu-central-1.amazonaws.com/tegra210-p3448-0002-s360v0-2-b00.dts --output tegra210-p3448-0002-s360v0-2-b00.dts 
+mv tegra210-p3448-0002-s360v0-2-b00.dts /boot/tegra210-p3448-0002-s360v0-2-b00.dts
 curl https://s360.s3.eu-central-1.amazonaws.com/set_mac.py --output set_mac.py
 python3 set_mac.py
+rm set_mac.py
+rm final.sh
+
